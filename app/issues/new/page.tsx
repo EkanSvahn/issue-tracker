5deaebf639bce9effc84fd805e408f8e9840d47e
EkanSvahn <ekan.svahn@gmail.com>
2024-12-10 16:11:41 +0100

    Feature: add new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div>NewIssuePage</div>
-  )
-}
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Enter a title">
+      </TextField.Root>
+        <TextArea></TextArea>
+        <Button>Submit</Button>
+    </div>
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
