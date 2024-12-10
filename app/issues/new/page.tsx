a0ac8b90d8a02ce2df6a70c0541c56fd9ffae0ed
EkanSvahn <ekan.svahn@gmail.com>
2024-12-10 16:16:29 +0100

    Feature: install react mde

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Enter a title">
-      </TextField.Root>
-        <TextArea></TextArea>
-        <Button>Submit</Button>
+      <TextField.Root placeholder="Enter a title"></TextField.Root>
+      <SimpleMDE />
+      <Button>Submit</Button>
     </div>
   );
 };
