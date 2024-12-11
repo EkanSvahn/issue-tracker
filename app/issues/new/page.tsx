cb04c0abc54ea9b2a7b436e71aa83f3c4c7d2894
EkanSvahn <ekan.svahn@gmail.com>
2024-12-11 13:20:38 +0100

    Feature: add error handling - handle errors - add radix callout on unexpected errors - add client side validation - extract error to a error component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,60 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
-import SimpleMDE from "react-simplemde-editor";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { createIssueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
+
+type IssueFormValues = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IssueFormValues>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+
+  const onSubmit: SubmitHandler<IssueFormValues> = async (data) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("An unexpected error occurred");
+    }
+  };
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Enter a title"></TextField.Root>
-      <SimpleMDE />
-      <Button>Submit</Button>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className=" space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder="Enter a title" {...register("title")} />
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
