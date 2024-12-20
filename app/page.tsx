24cafb6032fad8aa205d1d05502b951bab81cbba
EkanSvahn <ekan.svahn@gmail.com>
2024-12-20 11:30:28 +0100

    Feature: build layout of pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Pageination from "./components/Pageination";
 
 export default function Home() {
   return (
-    <main >
-     Hello World
-    </main>
-  )
+    <Pageination itemCount={100} pageSize={10} currentPage={2}></Pageination>
+  );
 }
